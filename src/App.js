9ed2021f3e5550507060750380d110d60283b41f
ltann <leeroyleeroy18@gmail.com>
2020-02-01 23:46:15 -0800

    Basic setup of React

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component }from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './App.css';
+
+// Pages
+import Home from './pages/home'
+import Login from './pages/login'
+import Signup from './pages/signup'
+
+class App extends Component {
+  render() {
+      return (
+          <div className="App">
+              <Router>
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/signup" component={Signup} />
+                </Switch>
+              </Router>
+          </div>
+      )
+  }
+}
+
+export default App
