fa1ced1abdc30cf9b8a8f6c266cbad22207a94c2
ltann <leeroyleeroy18@gmail.com>
2020-02-02 23:46:17 -0800

    Added @material-ui/core and created navbar

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import React, { Component }from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 
+// Components
+import Navbar from './components/Navbar'
+
 // Pages
-import Home from './pages/home'
-import Login from './pages/login'
-import Signup from './pages/signup'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 class App extends Component {
   render() {
       return (
           <div className="App">
               <Router>
-                <Switch>
-                  <Route exact path="/" component={Home} />
-                  <Route exact path="/login" component={Login} />
-                  <Route exact path="/signup" component={Signup} />
-                </Switch>
+                <Navbar />
+                <div className="container">
+                  <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/signup" component={Signup} />
+                  </Switch>
+                </div>           
               </Router>
           </div>
       )
