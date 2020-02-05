0c9eab3a8266ab8790c7e8cb308fece1d6ce49cc
ltann <leeroyleeroy18@gmail.com>
2020-02-05 00:01:24 -0800

    Added card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component }from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 
+//MUI
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import { createMuiTheme } from '@material-ui/core/styles'
+
 // Components
 import Navbar from './components/Navbar'
 
@@ -10,9 +14,28 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 
+
+const myTheme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f51b5',
+      dark: '#002884',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#fff'
+    }
+  }
+})
+
 class App extends Component {
   render() {
       return (
+        <MuiThemeProvider theme={myTheme}>
           <div className="App">
               <Router>
                 <Navbar />
@@ -25,6 +48,7 @@ class App extends Component {
                 </div>           
               </Router>
           </div>
+        </MuiThemeProvider>
       )
   }
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import Screams from '../components/Screams'
+
+import { Grid } from '@material-ui/core'
 
 export class Home extends Component {
+    state = {
+        screams: null
+    }
+
+    componentDidMount() {
+        axios.get('/screams')
+            .then(res => {
+                console.log(res.data)
+                this.setState({
+                    screams: res.data
+                })
+            })
+            .catch(err => console.log(err))
+    }
+
     render() {
+        // if the screams are there then load each scream. else display loading
+        let recentScreamsMarkup = this.state.screams ? (this.state.screams.map(scream => <Screams key={scream.screamId} scream={scream} />)) : <p>Loading...</p>
         return (
-            <div>
-                <h1>Home Page</h1>
-            </div>
+            <Grid container spacing={3}>
+                <Grid item sm={8} xs={12}>
+                    {recentScreamsMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile.....</p>
+                </Grid>
+            </Grid>           
         )
     }
 }
