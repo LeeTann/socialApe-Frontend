50993c7cb78118ab992bc80c9ece6bbb1c7fdbc0
ltann <leeroyleeroy18@gmail.com>
2020-02-05 22:55:28 -0800

    refactor home from class to functional w/ hooks

--- src/components/Screams.js
+++ src/components/Screams.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import dayjs, { locale } from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
 import withStyles from '@material-ui/core/styles/withStyles'
 
 import Card from '@material-ui/core/Card';
@@ -15,23 +18,25 @@ const styles = {
     }, 
     image: {
         minWidth: 200,
-        width: 200,
-        height: 200
+        backgroundPosition: 'top ',
     },
     content: {
         padding: 25,
-    }
+    },
 }
 
 export class Screams extends Component {
     render() {
+
+        dayjs.extend(relativeTime)
         const { classes, scream: {body, createAt, userImage, userHandle, screamId, likeCount, commentCount} } = this.props
+
         return (
             <Card className={classes.card}>
                 <CardMedia image={userImage} title="Profile image" className={classes.image} />
                 <CardContent className={classes.content}>
                     <Typography variant="h5" component={Link} to={`/users/${userHandle}`} color="primary" >{userHandle}</Typography>
-                    <Typography variant="body2" color="textSecondary" >{createAt}</Typography>
+                    <Typography variant="body2" color="textSecondary" >{dayjs(createAt).to(dayjs('Feb 1, 2020 8:00 PM'))}</Typography>
                     <Typography variant="body1" >{body}</Typography>
                 </CardContent>
             </Card>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,39 +1,34 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Screams from '../components/Screams'
 
 import { Grid } from '@material-ui/core'
 
-export class Home extends Component {
-    state = {
-        screams: null
-    }
+export const Home = props => {
 
-    componentDidMount() {
+    const [screams, setScreams] = useState(null)
+
+    useEffect(() => {
         axios.get('/screams')
-            .then(res => {
-                console.log(res.data)
-                this.setState({
-                    screams: res.data
-                })
-            })
-            .catch(err => console.log(err))
-    }
+        .then(res => {
+            setScreams(res.data)
+        })
+        .catch(err => console.log(err))
+    }, [])
 
-    render() {
-        // if the screams are there then load each scream. else display loading
-        let recentScreamsMarkup = this.state.screams ? (this.state.screams.map(scream => <Screams key={scream.screamId} scream={scream} />)) : <p>Loading...</p>
-        return (
-            <Grid container spacing={3}>
-                <Grid item sm={8} xs={12}>
-                    {recentScreamsMarkup}
-                </Grid>
-                <Grid item sm={4} xs={12}>
-                    <p>Profile.....</p>
-                </Grid>
-            </Grid>           
-        )
-    }
+    // if the screams are there then load each scream. else display loading
+    let recentScreamsMarkup = screams ? screams.map(scream => <Screams key={scream.screamId} scream={scream} />) : <p>Loading...</p>
+
+    return (
+        <Grid container spacing={3}>
+            <Grid item sm={8} xs={12}>
+                {recentScreamsMarkup}
+            </Grid>
+            <Grid item sm={4} xs={12}>
+                <p>Profile.....</p>
+            </Grid>
+        </Grid>           
+    )
 }
 
 export default Home
