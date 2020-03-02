41745f9eb37a28f5e4ed81b48f7ce1db554d0c49
ltann <leeroyleeroy18@gmail.com>
2020-03-01 21:05:52 -0800

    finished data actions

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -60,9 +60,7 @@ export const postScream = (newScream) => (dispatch) => {
                 type: POST_SCREAM,
                 payload: res.data
             })
-            dispatch({
-                type: CLEAR_ERRORS
-            })
+            dispatch(clearErrors())
         .catch(err => {
             dispatch({
                 type: SET_ERRORS,
@@ -94,4 +92,56 @@ export const unlikeScream = (screamId) => (dispatch) => {
             })
         })
     .catch(err => console.log(err))
+}
+
+// Submit a comment
+export const submitComment = (screamId, commentData) => (dispatch) => {
+    axios.post(`/scream/${screamId}/comment`, commentData)
+        .then((res) => {
+            dispatch({
+                type: SUBMIT_COMMENT,
+                payload: res.data
+            })
+            dispatch(clearErrors())
+        })
+        .catch((err) => {
+            dispatch({
+                type: SET_ERRORS,
+                payload: err.response.data
+            })
+        })
+}
+
+// Delete a scream
+export const deleteScream = (screamId) => (dispatch) => {
+    axios.delete(`/scream/${screamId}`)
+        .then(() => {
+            dispatch({
+                type: DELETE_SCREAM,
+                payload: screamId
+            })
+        })
+        .catch(err => console.log(err))
+}
+
+export const getUserData = (userHandle) => (dispatch) => {
+    dispatch({ type: LOADING_DATA })
+    
+    axios.get(`/user/${userHandle}`)
+        .then(res => {
+            dispatch({
+                type: SET_SCREAMS,
+                payload: res.data.screams
+            })
+        })
+        .catch(() => {
+            dispatch({
+                type: SET_SCREAMS,
+                payload: null
+            })
+        })
+}
+
+export const clearErrors = () => (dispatch) => {
+    dispatch({ type: CLEAR_ERRORS })
 }
